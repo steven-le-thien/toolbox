116275875007f4df607c052660582290be9151e8
thienle <steven.le.thien@gmail.com>
2019-09-07 13:22:06 -0400

    commenting on the test file

--- advanced_algo/fibonacci_heap/test.cpp
+++ advanced_algo/fibonacci_heap/test.cpp
@@ -3,6 +3,7 @@
 #include <limits>
 #include <iostream>
 
+// Make the private fields of FibHeap public to use this test unit
 int main(){
   advanced_algo::FibHeap *my_fibonacci_heap = new advanced_algo::FibHeap; 
 
